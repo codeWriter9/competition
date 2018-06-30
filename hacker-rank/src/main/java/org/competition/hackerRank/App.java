26d28175deead6227995154b111458a30858191a
Sanjay <Sanjay.X.Ghosh@gmail.com>
2018-06-30 14:55:28 +0530

    Adding a Hacker Rank project

---
+++ hacker-rank/src/main/java/org/competition/hackerRank/App.java
@@ -0,0 +1,13 @@
+package org.competition.hackerRank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
